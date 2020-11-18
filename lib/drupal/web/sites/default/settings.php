64cebfd5142600db6c108629ac00cfa07b1f9b39
Alex Verbruggen <verbruggenalex@gmail.com>
2020-11-18 21:04:11 +0000

    Test.

--- lib/drupal/web/sites/default/settings.php
+++ lib/drupal/web/sites/default/settings.php
@@ -5,6 +5,8 @@
  * This is the settings file.
  */
 
+$dbName = str_replace(['/home/project/', '/var/www/html/', '/lib/drupal/web/sites/default', '/', '.'], ['', '', '', '_', '-'], __DIR__);
+
 $databases['default']['default'] = [
   'database' => 'appointment',
   'driver' => 'mysql',
@@ -20,8 +22,29 @@ $settings['trusted_host_patterns'] = [
   '.*',
 ];
 
-// Use development config in dev environments.
-$config['config_split.config_split.config_dev']['status'] = (getenv('ENVIRONMENT') == 'development') ? TRUE : FALSE;
+if (getenv('ENVIRONMENT') == 'development') {
+  // Development configuration.
+  $config['config_split.config_split.config_dev']['status'] = TRUE;
+
+  // Use development services.
+  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+  // Disable css js aggregation.
+  $config['system.performance']['css']['preprocess'] = FALSE;
+  $config['system.performance']['js']['preprocess'] = FALSE;
+
+  // Disable caches.
+  $settings['cache']['bins']['render'] = 'cache.backend.null';
+  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+  $settings['cache']['bins']['page'] = 'cache.backend.null';
+
+  // Local SMTP settings.
+  $config['smtp.settings']['smtp_on'] = TRUE;
+  $config['smtp.settings']['smtp_host'] = 'smtp';
+  $config['smtp.settings']['smtp_username'] = '';
+  $config['smtp.settings']['smtp_password'] = '';
+}
+
 
 $settings['hash_salt'] = 'rFikc364CPkTMP22vy_PhbXU0HTq0U-jSmg5v22qpiKNNPnO3-6LQq4FG43GVFs4-L1vc1X1CA';
 
@@ -32,24 +55,6 @@ $config['locale.settings']['translation']['use_source'] = 'remote_and_local';
 // Used because the reverse proxy is on https and apache is on http.
 $settings['reverse_proxy'] = TRUE;
 $settings['reverse_proxy_addresses'] = [$_SERVER['REMOTE_ADDR']];
-
-// Use development services.
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
-
-// Disable css js aggregation.
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
-
-// Disable caches.
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-$settings['cache']['bins']['page'] = 'cache.backend.null';
-
-// Local SMTP settings.
-$config['smtp.settings']['smtp_on'] = TRUE;
-$config['smtp.settings']['smtp_host'] = 'smtp';
-$config['smtp.settings']['smtp_username'] = '';
-$config['smtp.settings']['smtp_password'] = '';
 
 
 // Test for production, not needed I think?
