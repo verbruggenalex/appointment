84f1432eb005a14079781239b6224fde1617ce81
Alex Verbruggen <verbruggenalex@gmail.com>
2020-11-29 19:48:08 +0000

    Debug settings.php

--- lib/scripts/deploy.sh
+++ lib/scripts/deploy.sh
@@ -9,14 +9,23 @@ TAG=$1
 
 # Build codebase and set pre-production environment
 rm -rf build/dist/${TAG} && mkdir -p build/dist/${TAG}
-if [ ! -f dist.tar.gz ]; then wget https://github.com/verbruggenalex/appointment/releases/download/${TAG}/dist.tar.gz; fi
+if [ ! -f dist.tar.gz ]
+  then
+  wget https://github.com/verbruggenalex/appointment/releases/download/${TAG}/dist.tar.gz
+fi
 tar -zxf dist.tar.gz -C build/dist/${TAG}
 ln -sfn ${BUILD_DIR}/dist/${TAG}/ ${BUILD_DIR}/pre-production
+composer set-permissions -d ${BUILD_DIR}/pre-production
+
+# Debug.
+cat ${BUILD_DIR}/pre-production/web/sites/default/settings.php
+ls -la cat ${BUILD_DIR}/pre-production/web/sites/default
 
 # Backup production and import on pre-production
 mkdir -p ${BACKUP_DIR}
 drush @prod sql-dump --result-file=${BACKUP_DIR}/production-${BACKUP_TIME}.sql
 ln -sfn ${BACKUP_DIR}/production-${BACKUP_TIME}.sql $(dirname ${BACKUP_DIR})/production-latest.sql
+drush cc drush
 drush @pre-prod sql-drop -y
 drush @pre-prod sql-create -y
 drush @pre-prod sqlc < ${BACKUP_DIR}/production-${BACKUP_TIME}.sql
