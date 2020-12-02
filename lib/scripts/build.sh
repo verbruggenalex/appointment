5dad4194edb5871db68d5cbbf3899ebe4ae1e548
Alex Verbruggen <verbruggenalex@gmail.com>
2020-12-02 19:44:20 +0000

    Test.

--- lib/scripts/build.sh
+++ lib/scripts/build.sh
@@ -1,32 +1,32 @@
 #!/bin/bash -x
 
 # Set variables.
-git fetch --tags
-LATEST_TAG=$(git describe --tags --abbrev=0)
+TAG=$1
 
 mkdir .tmp
-if [ ! -f .tmp/$LATEST_TAG.tar.gz ]
+if [ ! -f .tmp/$TAG.tar.gz ]
   then
-  wget https://github.com/verbruggenalex/appointment/releases/download/$LATEST_TAG/dist.tar.gz -O .tmp/$LATEST_TAG.tar.gz
+  wget https://github.com/verbruggenalex/appointment/releases/download/$TAG/dist.tar.gz -O .tmp/$TAG.tar.gz
 fi
 
-if [ ! -f .tmp/$LATEST_TAG.sql.gz ]
+if [ ! -f .tmp/$TAG.sql.gz ] && [ ! -f .tmp/$TAG.sql ]
   then
-  wget https://github.com/verbruggenalex/appointment/releases/download/$LATEST_TAG/clean.sql.gz -O .tmp/$LATEST_TAG.sql.gz
+  wget https://github.com/verbruggenalex/appointment/releases/download/$TAG/clean.sql.gz -O .tmp/$TAG.sql.gz
+  gunzip .tmp/$TAG.sql.gz
 fi
 
-if [ ! -d build/dist/$LATEST_TAG ]
+if [ ! -d build/dist/$TAG ]
   then
-  mkdir -p build/dist/$LATEST_TAG
-  tar -zxf .tmp/$LATEST_TAG.tar.gz -C build/dist/$LATEST_TAG
-  ln -sfn $APACHE_DOCUMENT_ROOT/build/dist/$LATEST_TAG $APACHE_DOCUMENT_ROOT/build/production
+  mkdir -p build/dist/$TAG
+  tar -zxf .tmp/$TAG.tar.gz -C build/dist/$TAG
+  ln -sfn $APACHE_DOCUMENT_ROOT/build/dist/$TAG $APACHE_DOCUMENT_ROOT/build/production
 fi
 
-if [ -f .tmp/$LATEST_TAG.sql.gz ]
+if [ -f .tmp/$TAG.sql ]
   then
   drush cc drush
   drush @prod sql-create -y
   drush @prod sql-drop -y
-  drush @prod sqlq --file=$APACHE_DOCUMENT_ROOT/.tmp/$LATEST_TAG.sql.gz
+  drush @prod sqlc < $APACHE_DOCUMENT_ROOT/.tmp/$TAG.sql
   drush @prod en app_default_content -y
 fi
