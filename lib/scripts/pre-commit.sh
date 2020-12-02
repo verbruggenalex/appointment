3ab4ae8620a349abecfa466e5378253241fad1a8
Alex Verbruggen <verbruggenalex@gmail.com>
2020-12-02 08:08:48 +0000

    Test git.

--- lib/scripts/pre-commit.sh
+++ lib/scripts/pre-commit.sh
@@ -1,17 +1,43 @@
 #!/bin/sh
 
-#
-# Run the hook command.
-# Note: this will be replaced by the real command during copy.
-#
+PROJECT=`php -r "echo dirname(dirname(dirname(realpath('$0'))));"`
+STAGED_FILES_CMD=`git diff --cached --name-only --diff-filter=ACMR HEAD | grep \\\\.php`
 
-# Fetch the GIT diff and format it as command input:
-DIFF=$(git -c diff.mnemonicprefix=false -c diff.noprefix=false --no-pager diff -r -p -m -M --full-index --no-color --staged | cat)
+# Determine if a file list is passed
+if [ "$#" -eq 1 ]
+then
+    oIFS=$IFS
+    IFS='
+    '
+    SFILES="$1"
+    IFS=$oIFS
+fi
+SFILES=${SFILES:-$STAGED_FILES_CMD}
 
-# Grumphp env vars
-export GIT_WORKING_DIR="$(git rev-parse --show-toplevel)"
+echo "Checking PHP Lint..."
+for FILE in $SFILES
+do
+    php -l -d display_errors=0 $PROJECT/$FILE
+    if [ $? != 0 ]
+    then
+        echo "Fix the error before commit."
+        exit 1
+    fi
+    FILES="$FILES $PROJECT/$FILE"
+done
 
-# Run GrumPHP
-(cd "${HOOK_EXEC_PATH}" && printf "%s\n" "${DIFF}" | "./vendor/bin/phpcs")
+if [ "$FILES" != "" ]
+then
+    echo "Running Code Sniffer. Code standard PSR2."
+    ./vendor/bin/phpcs --encoding=utf-8 -n -p $FILES
+    if [ $? ne 0 ]
+    then
+        echo "Fix the error before commit!"
+        echo "Run"
+        echo "  ./vendor/bin/phpcbf $FILES"
+        echo "for automatic fix or fix it manually."
+        exit 1
+    fi
+fi
 
-
+exit $?
