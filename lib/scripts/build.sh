33497fec7a2c58b4c7efaf4fc7c386ef7a2e7296
Alex Verbruggen <verbruggenalex@gmail.com>
2020-12-02 20:17:55 +0000

    Deploy.

--- lib/scripts/build.sh
+++ lib/scripts/build.sh
@@ -25,6 +25,10 @@ fi
 if [ -f .tmp/$TAG.sql ]
   then
   drush cc drush
+  drush @web sql-create -y
+  drush @web sql-drop -y
+  drush @web sqlc < $APACHE_DOCUMENT_ROOT/.tmp/$TAG.sql
+  drush @web en app_default_content -y
   drush @prod sql-create -y
   drush @prod sql-drop -y
   drush @prod sqlc < $APACHE_DOCUMENT_ROOT/.tmp/$TAG.sql
