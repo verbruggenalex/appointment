4f429f9ee1e857383779b7b24c94ffcf17416b79
Alex Verbruggen <verbruggenalex@gmail.com>
2020-12-04 20:08:40 +0000

    Retry.

--- lib/scripts/build.sh
+++ lib/scripts/build.sh
@@ -4,7 +4,7 @@
 TAG=$1
 
 mkdir .tmp
-if [ ! -f .tmp/$TAG.tar.gz ]
+if [ ! -d build/dist/$TAG ] && [ ! -f .tmp/$TAG.tar.gz ]
   then
   wget https://github.com/verbruggenalex/appointment/releases/download/$TAG/dist.tar.gz -O .tmp/$TAG.tar.gz
 fi
